075b1c04e4f6142fdf303367faeb40b7a4468f27
T0MAS2 <tomasbernimozzo@gmail.com>
2023-01-27 19:59:21 +0000

    msj

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Planetas, Favoritos, People, Vehicles
+import json
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +38,142 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def handle_user():
+    allusers = User.query.all()
+    print(allusers)
+    results = list(map(lambda item: item.serialize(),allusers))
+    print(results)
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(results), 200
 
-    return jsonify(response_body), 200
+@app.route('/user/<int:user_id>/favorites', methods=['GET'])
+def handle_favs(user_id):
+    favoritos_usuario = Favoritos.query.filter_by(id = user_id).first()
+    favs = favoritos_usuario.serialize()
+    # print(allplanetaid)
+    
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return jsonify(favs), 200
+
+@app.route('/people', methods=['GET'])
+def handle_people():
+    allpeople = People.query.all()
+    print(allpeople)
+    resultss = list(map(lambda item: item.serialize(),allpeople))
+    print(resultss)
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return jsonify(resultss), 200
+
+@app.route('/planetas', methods=['GET'])
+def handle_planetas():
+    allplanetas = Planetas.query.all()
+    print(allplanetas)
+    resplanet = list(map(lambda item: item.serialize(),allplanetas))
+    print(resplanet)
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return jsonify(resplanet), 200
+
+@app.route('/vlehices', methods=['GET'])
+def handle_vehicles():
+    allvehicles = Vehicles.query.all()
+    print(allvehicles)
+    resvehicles = list(map(lambda item: item.serialize(),allvehicles))
+    print(resvehicles)
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return jsonify(resvehicles), 200
+
+@app.route('/planetas/<int:planetas_id>', methods=['GET'])
+def handle_planetaid(planetas_id):
+    allplanetaid = Planetas.query.filter_by(id = planetas_id).first()
+    planets = allplanetaid.serialize()
+    # print(allplanetaid)
+    
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+
+    return jsonify(planets), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def handle_peopleid(people_id):
+    allpeopleid = People.query.filter_by(id = people_id).first()
+    
+    if allpeopleid is None: 
+        return jsonify("Persona no existe"), 404
+    # print(allpeopleid)
+    # response_body = {
+    #     "msg": "Hello, this is your GET /user response "
+    # }
+    people1 = allpeopleid.serialize()
+    return jsonify(people1), 200
+
+@app.route('/user/<int:user_id>/favorite/people', methods=['POST'])
+def agregar_people(user_id):
+    request_body = request.json #Guardo la respuesta que trae la solicitud en una variable que se llama "request_body" que es un objeto
+    print(request_body)
+    
+    print(request_body["people_id"]) #Accedo a la propiedad "people_id" del objeto "request_body" para obtener su valor
+    
+    new_favorite_people = Favoritos(planetas_id = None ,usuario_id = user_id, people_id = request_body["people_id"])
+    db.session.add(new_favorite_people)
+    db.session.commit()
+    return jsonify({"msg":"A sido agregado un nuevo favorito"}), 200
+
+
+
+@app.route('/user/<int:user_id>/favorite/planetas', methods=['POST'])
+def agregar_planetas(user_id):
+    request_body = request.json #Guardo la respuesta que trae la solicitud en una variable que se llama "request_body" que es un objeto
+    print(request_body)
+    
+    print(request_body["planetas_id"]) #Accedo a la propiedad "planetas_id" del objeto "request_body" para obtener su valor
+    
+    new_favorite_planetas = Favoritos(planetas_id = request_body["planetas_id"] ,usuario_id = user_id, people_id = None)
+    db.session.add(new_favorite_planetas)
+    db.session.commit()
+    return jsonify({"msg":"A sido agregado un nuevo favorito"}), 200
+
+@app.route('/user/<int:user_id>/favorite/planetas', methods=['DELETE'])
+def borrar_planetas(user_id):
+    request_body = request.json #Guardo la respuesta que trae la solicitud en una variable que se llama "request_body" que es un objeto
+    print(request_body)
+    
+    print(request_body["planetas_id"]) #Accedo a la propiedad "planetas_id" del objeto "request_body" para obtener su valor
+    
+    # buscar = Favoritos.query.filter_by(usuario_id = user_id, planetas_id = request_body["planetas_id"]).first()
+    buscar = Favoritos.query.filter_by(usuario_id = user_id, planetas_id = request_body["planetas_id"]).first()
+    db.session.delete(buscar)
+    db.session.commit()
+    return jsonify({"msg":"A sido eliminado un favorito"}), 200
+
+@app.route('/user/<int:user_id>/favorite/people', methods=['DELETE'])
+def borrar_people(user_id):
+    request_body = request.json #Guardo la respuesta que trae la solicitud en una variable que se llama "request_body" que es un objeto
+    print(request_body)
+    
+    print(request_body["people_id"]) #Accedo a la propiedad "people_id" del objeto "request_body" para obtener su valor
+    
+    buscar2 = Favoritos.query.filter_by(usuario_id = user_id, people_id = request_body["people_id"]).first()
+    db.session.delete(buscar2)
+    db.session.commit()
+    return jsonify({"msg":"A sido eliminado un favorito"}), 200
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -5,15 +5,116 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    nombre = db.Column(db.String(250), nullable=False)
+    apellido = db.Column(db.String(250), nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favoritos = db.relationship("Favoritos", backref='user')
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "nombre": self.nombre,
+            "is_active": self.is_active,
             # do not serialize the password, its a security breach
         }
+
+
+class People(db.Model):
+    __tablename__ = 'people'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False) 
+    birth_year = db.Column(db.String(250),nullable=False)
+    homeworld = db.Column(db.String(250), nullable=False)
+    starship = db.Column(db.String(250), nullable=False)
+    favoritos = db.relationship("Favoritos", backref='people')
+
+    def __repr__(self):
+        return '<People %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "birth_year": self.birth_year,
+            "homeworld": self.homeworld,
+            "starship": self.starship,
+            # do not serialize the password, its a security breac
+        }
+
+
+class Planetas(db.Model):
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    climate = db.Column(db.String (250))
+    created = db.Column(db.String (250))
+    diameter = db.Column(db.String (250))
+    favoritos = db.relationship("Favoritos", backref='planetas')
+
+    def __repr__(self):
+        return '<Planetas %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "climate": self.climate,
+            "created": self.created,
+            "diameter": self.diameter,
+            # do not serialize the password, its a security breac
+        }
+
+
+class Favoritos(db.Model):
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    # personajes_id = db.Column(db.Integer, db.ForeignKey('personajes.id'))
+    # vehiculos_id = db.Column(db.Integer, db.ForeignKey('vehiculos.id'))
+    planetas_id = db.Column(db.Integer, db.ForeignKey('planetas.id'))
+    usuario_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
+
+    def __repr__(self):
+        return '<Favoritos %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            # "personajes_id": self.personajes_id,
+            # "vehiculos_id": self.vehiculos_id,
+            "planetas_id": self.planetas_id,
+            "usuario_id": self.usuario_id,
+            "people_id": self.people_id,
+            # do not serialize the password, its a security breac
+        }
+
+class Vehicles(db.Model):
+    __tablename__ = 'vehicles'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False) 
+    model = db.Column(db.String(250), nullable=False)
+    vehicle_class = db.Column(db.String(250), nullable=False)
+    passengers = db.Column(db.String(250), nullable=False)
+
+    def __repr__(self):
+        return '<Vehicles %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "model": self.model,
+            "vehicles_class": self.vehicle_class,
+            "passengers": self.passengers,
+            # do not serialize the password, its a security breac
+        }
+
